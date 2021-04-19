cc1a236f17b10cbb7cea0ddcd9fcd4cec074cbc8
Magali <77433765+Liligamma@users.noreply.github.com>
2021-04-19 09:25:51 +0200

    implémentation jquery et slide du detail

--- src/main/resources/static/script/script.js
+++ src/main/resources/static/script/script.js
@@ -1,11 +1,24 @@
-const open = document.getElementById('1');
-const modal_container = document.getElementById('modal-container');
-const close = document.getElementById('close');
+// const open = document.getElementById('1');
+// const modal_container = document.getElementById('modal-container');
+// const close = document.getElementById('close');
+//
+// open.addEventListener('click' () =>{
+//     modal_container.classList.add('show');
+// });
+//
+// close.addEventListener('click' () =>{
+//     modal_container.classList.remove('show');
+// });
 
-open.addEventListener('click' () =>{
-    modal_container.classList.add('show');
-});
+$(document).ready(function() {
 
-close.addEventListener('click' () =>{
-    modal_container.classList.remove('show');
+    console.log ("tout est ok !");
+    //on localise l'objet .btn open et on écoute l'événement click
+    $("#open").click(function (){
+        $(".modal-container").slideDown(2000);
+    });
+
+    $("#close").click(function (){
+        $(".modal-container").hide();
+    });
 });
