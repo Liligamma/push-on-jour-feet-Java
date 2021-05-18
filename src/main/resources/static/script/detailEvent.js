8523b9b28a1c8159f00d53f29887599abd7222f7
Magali <77433765+Liligamma@users.noreply.github.com>
2021-05-18 08:37:08 +0200

    changing map

---
+++ src/main/resources/static/script/detailEvent.js
@@ -0,0 +1,10 @@
+var bMapAPIKey = AodddEr2Z6uTzo8eX5a7xaLk4OHojboX8Bj_wnZ5vxxk1D7H17BO89v-SEGR9HMe;
+
+ function GetMap()
+   {
+   "use strict";
+
+    var map = new Microsoft.Maps.Map('#myMap');
+
+   }
+
