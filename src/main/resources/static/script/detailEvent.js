d091732e7dc56ac2916994672a424381728b2451
Magali <77433765+Liligamma@users.noreply.github.com>
2021-05-21 07:51:56 +0200

    carte chargée

--- src/main/resources/static/script/detailEvent.js
+++ src/main/resources/static/script/detailEvent.js
@@ -1,3 +1,6 @@
+app.use(express.static('public'));
+app.use(express.static('files'));
+
 var bMapAPIKey = AodddEr2Z6uTzo8eX5a7xaLk4OHojboX8Bj_wnZ5vxxk1D7H17BO89v-SEGR9HMe;
 
 var latitude = document.getElementById('latitude');
